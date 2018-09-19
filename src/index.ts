b3bf91dbcf3a519ebe4e08be8f5b0207e50c462b
3846masa <3846masahiro+git@gmail.com>
2018-09-19 18:16:26 +0900

    perf: remove polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import DateProxy from './DateProxy';
-import stringRaw from './string-raw';
 
 type DateProxyType = DateProxy;
 type DateKeys = keyof DateProxy;
@@ -7,7 +6,7 @@ type DateProxyFunction = (date: DateProxy) => any;
 
 const fdate = (strArr: TemplateStringsArray, ...args: (DateKeys | DateProxyFunction)[]) => (date: Date) => {
   const proxy = new DateProxy(date);
-  return stringRaw(strArr, ...args.map((argv) => (typeof argv === 'string' ? proxy[argv] : argv(proxy))));
+  return String.raw(strArr, ...args.map((argv) => (typeof argv === 'string' ? proxy[argv] : argv(proxy))));
 };
 
 export default fdate;
