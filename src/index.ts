d9324201b1311fd22b6c88848dfbe7b6a25498a6
3846masa <3846masahiro+git@gmail.com>
2019-09-21 17:04:08 +0900

    Revert "perf: remove polyfill"
    This reverts commit b3bf91dbcf3a519ebe4e08be8f5b0207e50c462b.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import DateProxy from './DateProxy';
+import stringRaw from './string-raw';
 
 type DateProxyType = DateProxy;
 type DateKeys = keyof DateProxy;
@@ -6,7 +7,7 @@ type DateProxyFunction = (date: DateProxy) => any;
 
 const litdate = (strArr: TemplateStringsArray, ...args: (DateKeys | DateProxyFunction)[]) => (date: Date) => {
   const proxy = new DateProxy(date);
-  return String.raw(strArr, ...args.map((argv) => (typeof argv === 'string' ? proxy[argv] : argv(proxy))));
+  return stringRaw(strArr, ...args.map((argv) => (typeof argv === 'string' ? proxy[argv] : argv(proxy))));
 };
 
 export default litdate;
